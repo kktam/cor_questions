2ebe765b3e53f3938f836c696201aa58f907f401
Ka Tam <nkltam2003@gmail.com>
2017-12-02 10:46:38 -0800

    setup main view, and some template cleanup for question 9

--- question9/src/App.js
+++ question9/src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CalendarList from './scene/CalendarList';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <CalendarList />
+      </MuiThemeProvider>
     );
   }
 }
---
+++ question9/src/scene/CalendarList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class CalendarList extends Component {
+    render() {
+      return (      
+        <div className="App">
+          <header className="App-header">
+            <div className="App-title">Question 9</div>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      );
+    }
+  }
+  
+  export default CalendarList;
