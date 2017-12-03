38186fa6d6df7c995c2af4c62011ed1cf182aa17
Ka Tam <nkltam2003@gmail.com>
2017-12-02 16:24:41 -0800

    API call using CORS routine (draft)

--- question9/src/scene/CalendarList.js
+++ question9/src/scene/CalendarList.js
@@ -6,16 +6,22 @@ import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
 import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
 
+import createCORSRequest from './../api/cors';
 import { COR_CALENDAR_API_GETEVENTS } from './../api/const';
 
 class CalendarList extends Component {
   events = [];
   indexes = [];
+  test = false;
+  method = 'POST';
+  xhr = null; 
 
   constructor(props) {
     super(props)
-    this.getCalendarEvents = this.getCalendarEvents.bind(this)
-    this.getCalendarEventsSuccess = this.getCalendarEventsSuccess.bind(this)
+    this.test = this.props.test;
+    this.getCalendarEvents = this.getCalendarEvents.bind(this);
+    this.getCalendarEventsSuccess = this.getCalendarEventsSuccess.bind(this);
+    this.getCalendarEventsFailed = this.getCalendarEventsFailed.bind(this);   
     this.state = {
     };
 
@@ -25,17 +31,46 @@ class CalendarList extends Component {
   }
 
   getCalendarEvents(path) {
-    // during test use these test data
-    this.events = [ { name: "Brendan Lim", index: 1, image : "images/ok-128.jpg" } , 
-              { name: "Eric Hoffman", index: 2, image: "images/kolage-128.jpg" },
-              { name: "Grace Ng", index: 3, image: "images/uxceo-128.jpg" } ];
-    this.indexes = this.events.map( function (obj, index) {
-      return obj.name;
-    });
+    if (this.test === true) {
+      // during test use these test data
+      this.events = [{ name: "Brendan Lim", index: 1, image: "images/ok-128.jpg" },
+      { name: "Eric Hoffman", index: 2, image: "images/kolage-128.jpg" },
+      { name: "Grace Ng", index: 3, image: "images/uxceo-128.jpg" }];
+      this.indexes = this.events.map(function (obj, index) {
+        return obj.name;
+      });
+    } else {
+    
+      this.xhr = createCORSRequest(this.method, path); 
+      this.xhr.onload = this.getCalendarEventsSuccess;
+      this.xhr.onerror = this.getCalendarEventsFailed;
+
+      let postData = { value: 20 };
+      
+      // eslint-disable-next-line
+      let postDataStr = JSON.stringify(postData);      
+
+      this.xhr.setRequestHeader('Content-Type', 'application/json');  
+      //xhr.setRequestHeader('x-api-key', API_KEY);
+      this.xhr.send(postDataStr);      
+    }
   }
 
-  getCalendarEventsSuccess(result) {
+  getCalendarEventsSuccess() {
+    var result = this.xhr.responseText;
+    //var resultJson = JSON.parse(result);
+    // Success code goes here.
+    //console.log(`success:\n\n${JSON.stringify(result, null, 2)}`)
+
+    //this.events = resultJson;    
   }
+
+  getCalendarEventsFailed() {
+    var result = this.xhr.responseText;  
+    var resultJson = JSON.parse(result);      
+    // Error code goes here.
+    window.alert(`aws failed:\n\n${JSON.stringify(result, null, 2)}`)      
+  }  
 
   render() {
     return (
@@ -47,10 +82,10 @@ class CalendarList extends Component {
           <Subheader>Events</Subheader>
           {this.events.map(function (obj, index) {
             return <ListItem
-                      primaryText={obj.name}
-                      leftAvatar={<Avatar src={obj.image} />}
-                      rightIcon={<CommunicationChatBubble />}
-                    />
+              primaryText={obj.name}
+              leftAvatar={<Avatar src={obj.image} />}
+              rightIcon={<CommunicationChatBubble />}
+            />
           })}
         </List>
       </div>
